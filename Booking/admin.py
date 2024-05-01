e46365dd2f99a1d8aebf5afc38db47e5da83e186
keekz1 <148965858+keekz1@users.noreply.github.com>
2024-05-01 12:05:16 +0100

    updated Admin.py

--- Booking/admin.py
+++ Booking/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import Item, ToDoList
+from .models import Item, ToDoList,Category,Booking,Profile
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'lists','image','name','type','rental_period', 'description', 'complete')  # Include 'id' in list display
+    list_display = ('id','name','category', 'description','period','img')  # Include 'id' in list display
+     
+class ProfileAdmin(ImageCroppingMixin,admin.ModelAdmin):
+    pass
 
+admin.site.register(Category)
 admin.site.register(Item, ItemAdmin)
+admin.site.register(Booking)
+
+
+
 admin.site.register(ToDoList)
+admin.site.register(Profile,ProfileAdmin)
+
+
